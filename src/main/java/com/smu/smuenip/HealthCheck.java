74777263be4e7f5a2bd06fa1bc2c4150a46dabad
la1av1a <zldqkrktqk@gmail.com>
2023-03-15 16:09:58 +0900

    springBoot로 변경

---
+++ src/main/java/com/smu/smuenip/HealthCheck.java
@@ -0,0 +1,16 @@
+package com.smu.smuenip;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    private HttpStatus healthCheck(){
+        return HttpStatus.OK;
+    }
+}
---
+++ src/test/java/com/smu/smuenip/SmuEnipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.smuenip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuEnipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

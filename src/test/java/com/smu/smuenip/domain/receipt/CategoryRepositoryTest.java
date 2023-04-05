33585b781958c3432bf07372f43a33fce4ab4699
la1av1a <zldqkrktqk@gmail.com>
2023-04-05 15:03:02 +0900

    의미없는 테스트 코드 삭제

--- src/test/java/com/smu/smuenip/domain/receipt/CategoryRepositoryTest.java
+++ src/test/java/com/smu/smuenip/domain/receipt/CategoryRepositoryTest.java
@@ -3,12 +3,11 @@ package com.smu.smuenip.domain.receipt;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.smu.smuenip.domain.Category.model.Category;
 import com.smu.smuenip.domain.Category.model.CategoryRepository;
+import javax.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import javax.persistence.EntityManager;
 
 @DataJpaTest
 class CategoryRepositoryTest {
@@ -30,19 +29,6 @@ class CategoryRepositoryTest {
 
         Assertions.assertThat(category.getLargeCategory()).isEqualTo("짜장");
     }
-
-    @Test
-    void 조회_테스트2() {
-        Category category = new Category("커피1", "커피2", "커피3", "커피4");
-        categoryRepository.save(category);
-        Category category2 = new Category("커피1", "커피2", "커피3", null);
-
-        categoryRepository.save(category2);
-        Category result1 = categoryRepository.findCategoryByDetailCategory("커피4");
-
-        Assertions.assertThat(result1.getCategoryId()).isEqualTo(1L);
-        Assertions.assertThat(result1.getSubCategory()).isEqualTo("커피3");
-        Assertions.assertThat(result1.getDetailCategory()).isEqualTo("커피4");
-    }
+    
 
 }

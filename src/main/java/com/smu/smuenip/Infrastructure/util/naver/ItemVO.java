ad31fc9e997fb4a29efdaf6c578d02bd0bf54b7c
la1av1a <zldqkrktqk@gmail.com>
2023-04-06 04:19:59 +0900

    클래스명 변경 from ItemDTO.java

--- src/main/java/com/smu/smuenip/Infrastructure/util/naver/ItemVO.java
+++ src/main/java/com/smu/smuenip/Infrastructure/util/naver/ItemVO.java
@@ -1,24 +1,17 @@
 package com.smu.smuenip.Infrastructure.util.naver;
 
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
-public class ItemDTO {
+public class ItemVO implements Serializable {
 
     private String lastBuildDate;
     private int total;
     private int start;
     private int display;
     private List<Item> items = new ArrayList<>();
-
-    public ItemDTO(String lastBuildDate, int total, int start, int display, List<Item> items) {
-        this.lastBuildDate = lastBuildDate;
-        this.total = total;
-        this.start = start;
-        this.display = display;
-        this.items = items;
-    }
 }

f595ce72d0d3ed3f1135879abd19f94d084f6791
la1av1a <zldqkrktqk@gmail.com>
2023-04-08 17:20:25 +0900

    Receipt 엔티티 저장부분 리팩토링

--- src/main/java/com/smu/smuenip/domain/receipt/service/ReceiptService.java
+++ src/main/java/com/smu/smuenip/domain/receipt/service/ReceiptService.java
@@ -1,6 +1,11 @@
 package com.smu.smuenip.domain.receipt.service;
 
+import com.smu.smuenip.Infrastructure.config.exception.UnExpectedErrorException;
+import com.smu.smuenip.domain.image.Receipt;
+import com.smu.smuenip.domain.image.ReceiptRepository;
+import com.smu.smuenip.domain.user.model.User;
 import com.smu.smuenip.domain.user.repository.UserRepository;
+import com.smu.smuenip.enums.meesagesDetail.MessagesFail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +13,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReceiptService {
 
+    private final ReceiptRepository receiptRepository;
     private final UserRepository userRepository;
 
+    public void saveProductInfo(String imageUrl, Long userId) {
+        User user = findUserByUserId(userId);
 
+        Receipt receipt = Receipt.builder()
+            .imageUrl(imageUrl)
+            .user(user)
+            .build();
+        receiptRepository.save(receipt);
+    }
+
+    private User findUserByUserId(Long userId) {
+        return userRepository.findUserById(userId).orElseThrow(
+            () -> new UnExpectedErrorException(MessagesFail.USER_NOT_FOUND.getMessage()));
+    }
 }

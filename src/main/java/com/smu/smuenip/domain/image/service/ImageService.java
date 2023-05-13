81af9db9a96b2e5eb278111607498792adcf8b35
la1av1a <zldqkrktqk@gmail.com>
2023-05-13 23:36:19 +0900

    style : 사용하지 않는 (로컬에 저장하는 로직) 삭제

--- src/main/java/com/smu/smuenip/domain/image/service/ImageService.java
+++ src/main/java/com/smu/smuenip/domain/image/service/ImageService.java
@@ -1,20 +1,14 @@
 package com.smu.smuenip.domain.image.service;
 
-import com.smu.smuenip.Infrastructure.config.exception.UnExpectedErrorException;
 import com.smu.smuenip.Infrastructure.util.naver.PurchasedItemVO;
 import com.smu.smuenip.Infrastructure.util.naver.ocr.ocrResult.OcrResultDTO;
 import com.smu.smuenip.Infrastructure.util.s3.S3API;
-import com.smu.smuenip.enums.meesagesDetail.MessagesFail;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.imageio.ImageIO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mock.web.MockMultipartFile;
@@ -50,35 +44,6 @@ public class ImageService {
 
         return list;
 
-    }
-
-    public String saveImageInLocal(MultipartFile image) throws UnExpectedErrorException {
-        BufferedImage bufferedImage;
-        try {
-            bufferedImage = ImageIO.read(image.getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException("이미지를 읽는 도중 오류가 발생했습니다.", e);
-        }
-
-        if (image == null) {
-            throw new RuntimeException("이미지 파일이 올바르지 않거나 지원되지 않는 형식입니다.");
-        }
-        String outputFilePath = "uploads/" + image.getOriginalFilename();
-        File outputFile = new File(outputFilePath);
-
-        try {
-            boolean res = ImageIO.write(bufferedImage, getFileExtension(outputFile), outputFile);
-        } catch (IOException e) {
-            throw new UnExpectedErrorException(MessagesFail.IMAGE_UPLOAD_FAIL.getMessage());
-        }
-
-        return outputFilePath;
-    }
-
-    private String getFileExtension(File file) {
-        String fileName = file.getName();
-        int index = fileName.lastIndexOf('.');
-        return index > 0 ? fileName.substring(index + 1) : "";
     }
 
     public MultipartFile base64ToMultipartFile(String base64) {

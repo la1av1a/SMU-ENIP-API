da2dc7a2d445b6b5ed2c12bbf716cfe7fe9440b4
la1av1a <zldqkrktqk@gmail.com>
2023-05-20 21:41:03 +0900

    remove : 안 쓰는 클래스들 삭제

--- src/main/java/com/smu/smuenip/Infrastructure/config/redis/TokenInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smu.smuenip.Infrastructure.config.redis;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TokenInfoRepository extends CrudRepository<TokenInfo, String> {
-
-}

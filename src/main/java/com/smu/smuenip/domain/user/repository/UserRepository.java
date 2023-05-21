839042b1e148b1dfc4b69db31744eaada32e37c7
la1av1a <zldqkrktqk@gmail.com>
2023-05-21 15:01:18 +0900

    쿼리 수정

--- src/main/java/com/smu/smuenip/domain/user/repository/UserRepository.java
+++ src/main/java/com/smu/smuenip/domain/user/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.smu.smuenip.domain.user.repository;
 
 
 import com.smu.smuenip.domain.user.model.User;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -14,11 +15,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByUserId(Long id);
 
+    Optional<User> findUserByEmail(String email);
+
     boolean existsUsersByLoginId(String loginId);
 
     @Query(value = "SELECT u.login_id, u.score, RANK() OVER (ORDER BY score DESC) AS ranking "
-        + "FROM user u "
-        + "ORDER BY ranking "
-        + "LIMIT :size OFFSET :offset", nativeQuery = true)
+            + "FROM user u "
+            + "ORDER BY ranking "
+            + "LIMIT :size OFFSET :offset", nativeQuery = true)
     List<Object[]> findUserScore(@Param("size") int size, @Param("offset") int offset);
 }

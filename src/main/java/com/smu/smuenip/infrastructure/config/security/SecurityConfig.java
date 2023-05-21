227f3ff87ae2a4e82a9fa5718341ae26e1ef680d
la1av1a <zldqkrktqk@gmail.com>
2023-05-21 15:10:31 +0900

    줄바꿈 변경

--- src/main/java/com/smu/smuenip/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/com/smu/smuenip/infrastructure/config/security/SecurityConfig.java
@@ -20,33 +20,33 @@ public class SecurityConfig {
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     private final String[] swaggerURL = {
-        "/v3/api-docs", "/configuration/ui",
-        "/swagger-resources", "/configuration/security",
-        "/swagger-ui.html", "/webjars/**", "/swagger/**"
+            "/v3/api-docs", "/configuration/ui",
+            "/swagger-resources", "/configuration/security",
+            "/swagger-ui.html", "/webjars/**", "/swagger/**"
     };
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .cors().configurationSource(corsConfigurationSource)
-            .and()
-            .headers().frameOptions().disable()
-            .and()
-            .httpBasic().disable()
-            .formLogin().disable()
-            .authorizeRequests()
-            .antMatchers(swaggerURL).permitAll()
-            .antMatchers("/user/loginTest", "/upload/list").authenticated()
-            .antMatchers("/test").hasRole("ADMIN")
-            .anyRequest().permitAll()
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().exceptionHandling()
-            .authenticationEntryPoint(authenticationEntryPoint);
+                .cors().configurationSource(corsConfigurationSource)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .authorizeRequests()
+                .antMatchers(swaggerURL).permitAll()
+                .antMatchers("/user/loginTest", "/upload/list").authenticated()
+                .antMatchers("/test").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint);
 
         http.addFilterBefore(
-            jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
+                jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
         );
 
         return http.build();

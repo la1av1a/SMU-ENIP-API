dc57c4be301865874b01a7cac4c75466a73697a7
la1av1a <zldqkrktqk@gmail.com>
2023-05-23 16:09:27 +0900

    fix : NonUniqueResultException 해결. fetchOne -> fetchFirst

--- src/main/java/com/smu/smuenip/domain/category/model/CategoryRepositorySupport.java
+++ src/main/java/com/smu/smuenip/domain/category/model/CategoryRepositorySupport.java
@@ -1,15 +1,16 @@
 package com.smu.smuenip.domain.category.model;
 
-import static com.smu.smuenip.domain.category.model.QCategory.category;
-
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+import static com.smu.smuenip.domain.category.model.QCategory.category;
+
 @Repository
 public class CategoryRepositorySupport extends QuerydslRepositorySupport implements
-    CustomCategoryRepository {
+        CustomCategoryRepository {
 
     private final JPAQueryFactory queryFactory;
 
@@ -22,8 +23,8 @@ public class CategoryRepositorySupport extends QuerydslRepositorySupport impleme
     @Override
     public Optional<Category> findCategoryByCategoryId(Long categoryId) {
         Category category1 = queryFactory.selectFrom(category)
-            .where(category.categoryId.eq(1L))
-            .fetchOne();
+                .where(category.categoryId.eq(1L))
+                .fetchOne();
 
         return Optional.ofNullable(category1);
     }
@@ -31,21 +32,21 @@ public class CategoryRepositorySupport extends QuerydslRepositorySupport impleme
     @Override
     public Category findCategoryByDetailCategory(String detailCategory) {
         return queryFactory.selectFrom(category)
-            .where(category.detailCategory.eq(detailCategory))
-            .fetchOne();
+                .where(category.detailCategory.eq(detailCategory))
+                .fetchOne();
     }
 
     @Override
     public Category findCategoryByDetailCategoryOrSubCategory(String detailCategory,
-        String subCategory) {
+                                                              String subCategory) {
         Category categoryByDetail = queryFactory.selectFrom(category)
-            .where(category.detailCategory.eq(detailCategory))
-            .fetchOne();
+                .where(category.detailCategory.eq(detailCategory))
+                .fetchFirst();
 
         if (categoryByDetail == null) {
             return queryFactory.selectFrom(category)
-                .where(category.subCategory.eq(subCategory))
-                .fetchOne();
+                    .where(category.subCategory.eq(subCategory))
+                    .fetchFirst();
         }
 
         return categoryByDetail;

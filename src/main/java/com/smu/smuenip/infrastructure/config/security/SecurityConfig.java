3c4a288414330292bc7757b2d445c259fd7bea42
la1av1a <zldqkrktqk@gmail.com>
2023-05-31 04:18:03 +0900

    fix : swagger 문서 접속이 안 되던 오류 수정

--- src/main/java/com/smu/smuenip/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/com/smu/smuenip/infrastructure/config/security/SecurityConfig.java
@@ -20,36 +20,36 @@ public class SecurityConfig {
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     private final String[] swaggerURL = {
-            "/v3/api-docs", "/configuration/ui",
-            "/swagger-resources", "/configuration/security",
-            "/swagger-ui.html", "/webjars/**", "/swagger/**"
+        "/v3/api-docs", "/configuration/ui",
+        "/swagger-resources", "/configuration/security",
+        "/swagger-ui.html", "/webjars/**", "/swagger/**",
+        "/swagger-ui/**", "/swagger-resources/**"
     };
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .cors().configurationSource(corsConfigurationSource)
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .httpBasic().disable()
-                .formLogin().disable()
-                .authorizeRequests()
-                .antMatchers(swaggerURL).permitAll()
-                .antMatchers("/user/rank/**", "/", "/signUp", "/login").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint);
+            .cors().configurationSource(corsConfigurationSource)
+            .and()
+            .headers().frameOptions().disable()
+            .and()
+            .httpBasic().disable()
+            .formLogin().disable()
+            .authorizeRequests()
+            .antMatchers(swaggerURL).permitAll()
+            .antMatchers("/user/rank/**", "/", "/signUp", "/login").permitAll()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().exceptionHandling()
+            .authenticationEntryPoint(authenticationEntryPoint);
 
         http.addFilterBefore(
-                jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
+            jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
         );
 
         return http.build();
     }
-
 }

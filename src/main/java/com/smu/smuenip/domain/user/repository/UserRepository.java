7889bf6d927be72ffacbe9085b1acf314f4a612b
la1av1a <zldqkrktqk@gmail.com>
2023-06-05 14:34:31 +0900

    delete : 안 쓰는 코드 삭제

--- src/main/java/com/smu/smuenip/domain/user/repository/UserRepository.java
+++ src/main/java/com/smu/smuenip/domain/user/repository/UserRepository.java
@@ -3,10 +3,7 @@ package com.smu.smuenip.domain.user.repository;
 
 import com.smu.smuenip.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -15,15 +12,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByEmail(String email);
 
-
-    /**
-     * @param size
-     * @param offset
-     * @return nickName, score, rank, userId
-     */
-    @Query(value = "SELECT u.nick_name, u.score, RANK() OVER (ORDER BY score DESC) AS ranking "
-            + "FROM user u "
-            + "ORDER BY ranking "
-            + "LIMIT :size OFFSET :offset", nativeQuery = true)
-    List<Object[]> findUserScore(@Param("size") int size, @Param("offset") int offset);
 }

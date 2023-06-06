ce5872671b49e216a5075cc934bdac8a5520c9e6
la1av1a <zldqkrktqk@gmail.com>
2023-06-07 01:03:51 +0900

    이메일 또는 아이디가 존재하면 가입 못하게 변경

--- src/main/java/com/smu/smuenip/domain/user/repository/UserAuthRepository.java
+++ src/main/java/com/smu/smuenip/domain/user/repository/UserAuthRepository.java
@@ -3,9 +3,8 @@ package com.smu.smuenip.domain.user.repository;
 import com.smu.smuenip.domain.user.model.User;
 import com.smu.smuenip.domain.user.model.UserAuth;
 import com.smu.smuenip.enums.Provider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserAuthRepository extends JpaRepository<UserAuth, Long> {
 
@@ -16,4 +15,7 @@ public interface UserAuthRepository extends JpaRepository<UserAuth, Long> {
     Optional<UserAuth> findUserAuthByUserAndProvider(User user, Provider provider);
 
     boolean existsUserAuthByProviderIdAndProvider(String providerId, Provider provider);
+
+    boolean existsUserAuthsByProviderIdOrUserEmailAndProvider(String providerId, String email,
+        Provider provider);
 }
